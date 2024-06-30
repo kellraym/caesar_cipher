965d8b38e2bc0ba69e4922cc3ae534c80688e145
Ray <ray.al.kell@gmail.com>
2024-06-30 10:55:11 -0600

    encrypts single input by static rotation

--- main.c
+++ main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
-int main()
+#define UPPER_BOTTOM 65
+#define UPPER_TOP 90
+#define LOWER_BOTTOM 97
+#define LOWER_TOP 122
+
+// TODO rotate chars
+// output encrypted/decrypted message
+
+int rotate(int c, int r, int bottom)
 {
-    int ch;
-    while ((ch = getchar()) != EOF)
+    return (c - bottom + r) % 26 + bottom;
+}
+
+// rotates input int c by r or returns non-alpha c
+int encrypt(int c, int r)
+{
+    if (c >= UPPER_BOTTOM && c <= UPPER_TOP)
     {
-        putchar(ch);
+        return rotate(c, r, UPPER_BOTTOM);
     }
+    else if (c >= LOWER_BOTTOM && c <= LOWER_TOP)
+    {
+        return rotate(c, r, LOWER_BOTTOM);
+    }
+
+    return c;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Missing arguments!");
+        exit(1);
+    }
+
+    int ch;
+    for (int i = 0; i < strlen(argv[1]); ++i)
+    {
+        putchar(encrypt(argv[1][i], 3));
+    }
+    return EXIT_SUCCESS;
 }
